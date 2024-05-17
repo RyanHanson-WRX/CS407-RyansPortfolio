cc96c29a0666915e697c8686f00bb2382ed602ba
Ryan Hanson <rhanson19@mail.wou.edu>
2024-05-16 20:53:38 -0700

    Fixed import

--- RyansPortfolio/src/routes/assignment6/src/World/components/fps/rifle.ts
+++ RyansPortfolio/src/routes/assignment6/src/World/components/fps/rifle.ts
@@ -1,16 +1,16 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
 import { setupModel } from './setupModel.ts';
 
+
 async function loadRifle() {
-  const loader = new GLTFLoader();
-  
-  const rifleData = await loader.loadAsync('src/routes/assignment6/src/World/components/assets/animated_rifle/scene.gltf');
+    const loader = new GLTFLoader();
+    
+    const rifleData = await loader.loadAsync('/src/routes/assignment6/src/World/components/assets/animated_rifle/scene.gltf');
 
 
-  const rifle = setupModel(rifleData);
+    const rifle = setupModel(rifleData);
 
-  return rifle;
+    return rifle;
 }
 
 export { loadRifle };
